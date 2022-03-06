5feaf425c19668380fe95b7800d213078fcdfc52
SandraAdel <81769303+SandraAdel@users.noreply.github.com>
2022-03-07 01:01:41 +0200

    Adding General Variables Initialization in main.py

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # Variables Initialization
+        self.isChannel1Open, self.isChannel2Open, self.isChannel3Open = False, False, False
+        self.isChannel1Shown, self.isChannel2Shown, self.isChannel3Shown = False, False, False
+        self.channel1Signal = pyqtgraph.PlotCurveItem()
+        self.channel1TimeReadings = []
+        self.channel1AmplitudeReadings = []
+        self.channel1PlottedXCoordinates = []
+        self.channel1PlottedYCoordinates = []
+        self.channel2Signal = pyqtgraph.PlotCurveItem()
+        self.channel2TimeReadings = []
+        self.channel2AmplitudeReadings = []
+        self.channel2PlottedXCoordinates = []
+        self.channel2PlottedYCoordinates = []
+        self.channel3Signal = pyqtgraph.PlotCurveItem()
+        self.channel3TimeReadings = []
+        self.channel3AmplitudeReadings = []
+        self.channel3PlottedXCoordinates = []
+        self.channel3PlottedYCoordinates = []
 
         # Links of GUI Elements to Methods:
 
